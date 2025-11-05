989c007283319a9636ebd23e33eb2c5e854ac0cd
syahroni7 <muhamadsyahroni7@gmail.com>
2025-11-05 11:09:29 +0700

    menambahkan struktur

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Role::create(['name' => 'Administrator']);
+        Role::create(['name' => 'Editor']);
+        Role::create(['name' => 'Author']);
+        Role::create(['name' => 'Kontributor']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Roles Permission
+Route::prefix('admin')
+    ->middleware(['auth', 'role:Administrator|Editor|Author|Kontributor'])
+    ->group(function () {
+        Route::resource('posts', PostController::class);
+        Route::resource('categories', CategoryController::class);
+        Route::resource('users', UserController::class)
+            ->middleware('role:Administrator');
+    });
+
+    // Publik
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/berita/{slug}', [PostController::class, 'show'])->name('berita.show');
+Route::get('/kategori/{slug}', [CategoryController::class, 'show'])->name('kategori.show');
+
